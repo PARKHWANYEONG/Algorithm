635c1685a82686b56a1cc7f1b54eb99eb31e349e
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-13 18:21:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 44.14 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,9 +1,4 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-    for(let i=0; i<participant.length; i++){
-        if(participant[i] !== completion[i]){
-            return participant[i]
-        }
-    }
+    completion.map(v=>completion[v]= (completion[v]|0) + 1)
+   return participant.find(v=> !completion[v]--)
 }
