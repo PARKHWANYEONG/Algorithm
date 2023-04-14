901f3c12daf7ba10797b065e34965b2144e7acf8
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-14 18:11:44 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,8 +1,8 @@
 function solution(n) {
-    let arr = Array.from({length:n+1});
-    arr[0] = 1; 
-    arr[1] = 1;
-    for(let i=2; i<arr.length; i++){
+    let arr = []
+    arr[1] = 1;    
+    arr[2] = 2;
+    for(let i=3; i<=n; i++){
          arr[i] = (arr[i-2]+arr[i-1])%1234567;
     }
     return arr[n]
