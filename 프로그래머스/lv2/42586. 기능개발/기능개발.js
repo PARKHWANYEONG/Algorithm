e6b4008b975e0fc6177187c348c23185ff7f8ee8
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-15 18:34:38 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,17 +1,16 @@
-function solution(progresses, speeds) {
-    let day =progresses.map(v=> 100-v).map((val,i)=>Math.ceil(val/speeds[i]));
-    let criteria = day[0];
+function solution(P, S) {
     let answer = [];
     
-    for(let i=0, j=0; i< day.length; i++){
-       if(day[i]<=criteria){
-           answer[j]= (answer[j]||0) +1;
-       }else{
-           j++;
-           criteria = day[i];
-           answer[j]=(answer[j]||0) +1;
-       }
+    while(S.length){
+        S.forEach((v,i)=> P[i]+=S[i]);
+        let cnt = 0;
+        
+        while(P[0]>=100){
+            P.shift();
+            S.shift();
+            cnt++;
+        }
+        if(cnt>0) answer.push(cnt);
     }
-    console.log(day)
-    return answer
+    return answer;
 }
