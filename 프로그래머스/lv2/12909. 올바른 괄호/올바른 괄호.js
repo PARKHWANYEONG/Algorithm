39eaf35138d0c6990f64fd825755f8289d3b8941
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-20 22:22:03 +0900

    [level 2] Title: 올바른 괄호, Time: 2.92 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,26 +1,14 @@
 function solution(s){
-    let cnt = 0;
+    if(s[0] === ')') return false;
+    let temp = 0
+  
     for(let i=0; i<s.length; i++){
-        if(s[i]==='('){
-            cnt++;
-        }else{
-            cnt--;
-        }
+      if(s[i]==='('){
+          temp++;
+      }else{
+          temp--;
+      }
+      if(temp < 0) return false;
     }
-    if(cnt!==0) return false
-    
-    let result = [];
-    
-    for(let v of s){
-        if(v==='('){
-            result.push(v);
-        }else{
-            if(result.length===0){
-                return false;
-            }
-            result.pop();
-        }
-    }
-    return true;
+    return temp ? false : true
 }
-
