554b271819b51d66dc1cc185a0d8b738fb1d666c
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-20 23:55:33 +0900

    [level 2] Title: 짝지어 제거하기, Time: 25.72 ms, Memory: 46.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,11 +1,12 @@
 function solution(s){
-   let arr = [];
+    let temp = [];
+    
     for(let i=0; i<s.length; i++){
-        if(arr[arr.length-1]===s[i]){
-            arr.pop();
-        }else {
-            arr.push(s[i]);
+        if(temp[temp.length-1] === s[i]){
+            temp.pop();
+        }else{
+            temp.push(s[i])
         }
     }
-    return arr.length ? 0 : 1
+    return temp.length ? 0 : 1
 }
