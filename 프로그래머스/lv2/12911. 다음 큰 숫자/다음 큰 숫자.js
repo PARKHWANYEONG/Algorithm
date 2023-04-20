b06849b2871db0b03c1f45ee1c1416d330239c05
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-20 23:39:10 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,11 +1,8 @@
 function solution(n) {
-    let oneCnt = n.toString(2).split('').filter(v=> v === '1').length;
+    let cnt = n.toString(2).match(/1/g).length;
+    
     while(true){
         n++;
-        let temp = n.toString(2).split('').filter(v=> v === '1').length;
-        if(temp ===oneCnt){
-            return n
-        }
+        if(cnt === n.toString(2).match(/1/g).length) return n
     }
-    
 }
