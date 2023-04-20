3a0aec54f6c9967e4d53d13b73544950e1dbcf8f
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-21 03:25:10 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,9 +1,9 @@
 function solution(n,a,b){
-   let round = 0;
+   let answer = 0;
     while(a!==b){
-        a=Math.ceil(a/2)
-        b=Math.ceil(b/2);
-        round++;
+        a= Math.ceil(a/2)
+        b= Math.ceil(b/2)
+        answer++;
     }
-    return round
+    return answer;
 }
