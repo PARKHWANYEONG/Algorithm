2ad914b430645ab709bd28dadcf5be5ffccf8257
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-21 05:17:19 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,15 +1,24 @@
 function solution(s) {
-  let answer = 0;
-  for (let i = 0; i < s.length; i++) {
-    const arr = [];
-    const temp = i === 0 ? s : s.slice(i) + s.slice(0, i);
-    for (let j = 0; j < temp.length; j++) {
-      if (arr[arr.length - 1] === '(' && temp[j] === ')') arr.pop();
-      else if (arr[arr.length - 1] === '[' && temp[j] === ']') arr.pop();
-      else if (arr[arr.length - 1] === '{' && temp[j] === '}') arr.pop();
-      else arr.push(temp[j]);
+    var answer = 0;
+    
+    for(let i=0; i<s.length; i++){
+        let temp = s.slice(i) + s.slice(0,i);
+        let stack = [];
+        let result = true;
+        if(temp[0] === ')'|| temp[0] === '}'|| temp[0] === ']') continue;
+        for(let j=0; j<s.length; j++){
+            if(temp[j] === '{'||temp[j] === '['||temp[j] === '('){
+                stack.push(temp[j]);
+            }else{
+                let temp2 = stack.pop();
+                if(temp2 === '{' && temp[j] === '}') continue;
+                if(temp2 === '(' && temp[j] === ')') continue;
+                if(temp2 === '[' && temp[j] === ']') continue;
+                result = false;
+                break;
+            }
+        }
+        if(result) answer++
     }
-    if (arr.length === 0) answer++;
-  }
-  return answer;
+    return s.length%2 ? 0 : answer
 }
