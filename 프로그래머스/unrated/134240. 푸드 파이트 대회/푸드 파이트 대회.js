a9404186113ee74cc4f1cbb9ea8e16b15713d79b
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-22 01:01:37 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,13 +1,9 @@
 function solution(food) {
+    let answer = '0'
     
-    let arr = [0];
-    
-    for(let i=food.length-1; i>0; i--){
-        let temp = Math.floor(food[i]/2);
-        for(let j=0; j<temp; j++){
-            arr.unshift(i);
-            arr.push(i);
-        }
+    for(let i=food.length-1; i>0;  i--){
+        answer = (i+'').repeat(Math.floor(food[i]/2)) + answer +  (i+'').repeat(Math.floor(food[i]/2))
     }
-    return arr.join('')
+    
+    return answer
 }
