9bac4af85ec35bf5a4d54bbd732852a9e7aac11d
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-22 01:18:18 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,11 +1,9 @@
 function solution(a, b) {
-    let day = ['FRI','SAT','SUN','MON','TUE','WED','THU'];
     let month = [31,29,31,30,31,30,31,31,30,31,30,31];
+    let day = ['FRI','SAT','SUN','MON','TUE','WED','THU'];
     let sum = b;
-    for(let i=1; i<a; i++){
-        sum += month[i-1];
+    for(let i=0; i<a-1; i++){
+        sum += month[i];
     }
-    let gap = (sum-1)%7
-    return day[gap]
-
+    return day[--sum%7]
 }
