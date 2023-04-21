1c4a14e14dc597b9ff005f803621194b2cae67d6
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-22 01:39:52 +0900

    [level 1] Title: 모의고사, Time: 3.08 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,31 +1,18 @@
 function solution(answers) {
-    let one = [1,2,3,4,5];
-    let two = [2,1,2,3,2,4,2,5];
-    let thr = [3,3,1,1,2,2,4,4,5,5];
-    let ob={}
+    let one = [1, 2, 3, 4, 5];
+    let two = [2, 1, 2, 3, 2, 4, 2, 5,];
+    let thr = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    let score1 = 0,score2 = 0,score3 = 0;
     
-    answers.forEach((v,i)=>{
-        if(v=== one[i%one.length]){
-            ob[1] ? ob[1]++ : ob[1]=1
-        }
-        if(v===two[i%two.length]){
-            ob[2] ? ob[2]++ : ob[2]=1
-        }
-        if(v===thr[i%thr.length]){
-            ob[3] ? ob[3]++ : ob[3]=1
-        }
-    })
-    let answer = [];
-    let max=0;
-    for(let key in ob){
-        if(+ob[key]>=max){
-            max = +ob[key];
-        }
+    for(let i=0; i<answers.length; i++){
+          if(answers[i] === one[i%one.length]) score1++;
+          if(answers[i] === two[i%two.length]) score2++;
+          if(answers[i] === thr[i%thr.length]) score3++;
     }
-    for(let key in ob){
-        if(ob[key]===max){
-           answer.push(+key)
-        }
-    }
-    return answer.sort((a,b)=>a-b)
+    let answer = []
+    let max = Math.max(score1,score2,score3);
+    if(max === score1) answer.push(1);
+    if(max === score2) answer.push(2);
+    if(max === score3) answer.push(3);
+    return answer;
 }
