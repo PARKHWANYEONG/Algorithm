9599b7d021dce9e37498a10dec83ff640e4f3d77
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-24 17:52:24 +0900

    [unrated] Title: A 강조하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,15 @@
+function solution(myString) {
+    let answer = ''
+    
+    for(let i=0; i<myString.length; i++){
+        if(myString[i] === 'a'){
+            answer+= myString[i].toUpperCase();
+        }else if(myString[i] !== 'A'){
+            answer+= myString[i].toLowerCase();
+        }else{
+            answer+= myString[i]
+        }
+    }
+    
+    return answer
+}
