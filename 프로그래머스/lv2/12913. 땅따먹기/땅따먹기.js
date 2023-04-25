7e1a8653f7cd29a78220bfdd930830d31eabddd6
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-25 23:10:41 +0900

    [level 2] Title: 땅따먹기, Time: 77.51 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,10 @@
+function solution(land) {
+    
+    for(let i=1; i<land.length; i++){
+        for(let j=0; j<4; j++){
+            land[i][j] += Math.max(...land[i-1].filter((v,i)=> i!==j));
+        }
+    }
+    return Math.max(...land[land.length-1])
+}
+    
