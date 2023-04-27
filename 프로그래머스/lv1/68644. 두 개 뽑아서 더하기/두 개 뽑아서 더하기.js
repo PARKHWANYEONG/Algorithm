cc31593b917a6a2b0f9ff995822eeb0fa3181a15
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-28 05:50:52 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.49 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -2,19 +2,19 @@ function solution(numbers) {
     let set = new Set();
     let check = Array.from({length:numbers.length},()=>0);
     
-    const DFS = (idx,sum,cnt)=>{
+    const DFS = (sum,cnt)=>{
         if(cnt === 2){
            return set.add(sum);
         }
-        for(let i=idx; i<numbers.length; i++){
+        for(let i=0; i<numbers.length; i++){
             if(!check[i]){
                 check[i] = 1;
-                DFS(i,sum+numbers[i],cnt+1);
+                DFS(sum+numbers[i],cnt+1);
                 check[i] = 0;
             }
         }
     }
     
-    DFS(0,0,0);
+    DFS(0,0);
     return [...set].sort((a,b)=>a-b)
 }
