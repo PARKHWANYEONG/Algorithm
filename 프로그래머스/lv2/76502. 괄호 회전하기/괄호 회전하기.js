8369f5ed52ed1339dbef7b69cd2e7b59fddb0b96
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-28 00:14:31 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,24 +1,24 @@
 function solution(s) {
-    var answer = 0;
+    let stack = [];
+    let answer = 0;
     
     for(let i=0; i<s.length; i++){
+        let check = true;
         let temp = s.slice(i) + s.slice(0,i);
-        let stack = [];
-        let result = true;
-        if(temp[0] === ')'|| temp[0] === '}'|| temp[0] === ']') continue;
-        for(let j=0; j<s.length; j++){
-            if(temp[j] === '{'||temp[j] === '['||temp[j] === '('){
-                stack.push(temp[j]);
-            }else{
+        if(temp[0] === ')' || temp[0] === ']' || temp[0] === '}') continue;
+        for(let j=0; j<temp.length; j++){
+            if(temp[j] === '(' || temp[j] === '[' || temp[j] === '{') stack.push(temp[j]);
+            else{
                 let temp2 = stack.pop();
                 if(temp2 === '{' && temp[j] === '}') continue;
-                if(temp2 === '(' && temp[j] === ')') continue;
                 if(temp2 === '[' && temp[j] === ']') continue;
-                result = false;
+                if(temp2 === '(' && temp[j] === ')') continue;
+                check = false;
                 break;
             }
         }
-        if(result) answer++
+        if(check) answer++;
     }
     return s.length%2 ? 0 : answer
 }
+
