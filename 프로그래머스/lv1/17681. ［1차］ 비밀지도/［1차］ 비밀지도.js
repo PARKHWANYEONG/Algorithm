dadc2ec23bcd58a93755dddb8ce210c042f78cd4
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-28 04:39:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,5 +1,10 @@
 function solution(n, arr1, arr2) {
-   let a1 = arr1.map((v,i)=>+v.toString(2) + +arr2[i].toString(2)+'')
-   return a1.map(v=> v.length === n ? v : '0'.repeat(n-v.length)+v).map(v=>v.replace(/[1-2]/g,'#').replace(/0/g,' '))
-   
+    const rep = (str)=>{
+        return str.replace(/2|1|0/g,v=> +v>0 ? '#' : ' ');
+    }
+    
+   let a1 = arr1.map((v,i)=> +v.toString(2) + +arr2[i].toString(2) +'')
+   let answer = a1.map(v=> rep('0'.repeat(n-v.length)+v))
+    
+    return answer
 }
