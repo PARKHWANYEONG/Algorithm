3bc568dede78cf0d186e48b38743fe881a9d132a
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-27 21:34:50 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,8 +1,11 @@
 function solution(brown, yellow) {
-    let sum = brown+yellow;
-    let width;
-    for(let height=3; ; height++){
-        if(!(sum%height)) width= sum/height;
-        if((width-2)*(height-2) === yellow) return [width,height];
+    let sum = brown + yellow;
+    
+    for(let 가로=3; ; 가로++){
+        let 세로 = sum / 가로;
+        if(세로 > 가로 || 세로%1 !== 0) continue;
+        if((가로-2) * (세로-2) === yellow){
+            return [가로,세로]
+        }
     }
 }
