0efad5067954850cc6e0d23e9e6e6708ed8de1fb
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-27 20:59:28 +0900

    [level 2] Title: 피보나치 수, Time: 5.19 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,7 +1,8 @@
 function solution(n) {
-    let temp = [0,1,1];
+    let table = [0,1,1];
+    
     for(let i=3; i<=n; i++){
-        temp[i] = temp[i-1]%1234567 + temp[i-2]%1234567
+        table[i] = (table[i-1]%1234567 + table[i-2]%1234567)%1234567
     }
-    return temp[n]%1234567
+    return table[n]    
 }
