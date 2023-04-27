0f3514acf20cddffc5295ec5f692385083a0479d
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-28 03:06:52 +0900

    [level 1] Title: 삼총사, Time: 0.42 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,12 +1,20 @@
 function solution(number) {
+    let check = Array.from({length:number.length},()=>0);
     let answer = 0;
-    
-    for(let i=0; i<number.length-2; i++){
-        for(let j=i+1; j<number.length-1; j++){
-            for(let k=j+1; k<number.length; k++){
-                if(!(number[i]+number[j]+number[k])) answer++
+    const DFS = (start,sum,cnt)=>{
+        if(cnt === 3 && sum === 0){
+           answer++
+        }
+        
+        for(let i=start; i<number.length; i++){
+            if(!check[i]){
+                check[i] = 1;
+                DFS(i,sum+number[i],cnt+1);
+                check[i] = 0;
             }
         }
     }
+    
+    DFS(0,0,0);
     return answer
 }
