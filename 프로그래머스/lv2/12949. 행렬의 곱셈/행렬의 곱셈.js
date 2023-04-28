6d011402f7deacd0f085881eb86130f96d908420
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-29 01:17:45 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.56 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,15 +1,13 @@
-function solution(arr1, arr2) {
-    let answer = []
-   for(let i=0; i<arr1.length; i++){
-       let temp = [];
-       for(let j=0; j<arr2[0].length; j++){
-           let sum = 0;
-           for(let k=0; k<arr2.length; k++){
-               sum+= arr1[i][k]*arr2[k][j];
-           }
-           temp.push(sum);
-       }
-       answer.push(temp)
-   }
-    return answer;
+function solution(A, B) {
+    return A.map(val => val.map((v,i2)=> val.reduce((a,c,i)=> a+c*B[i][i2],0)))
+    
+   
 }
+function solution(A, B) {
+    return A.map((row) => B[0].map((x,y) => row.reduce((a,b,c) => a + b * B[c][y], 0)))
+}
+
+
+// [[2, 3, 2],  [[5, 4, 3]            
+//  [4, 2, 4],   [2, 4, 1]
+//  [3, 1, 4]]   [3, 1, 1]]
