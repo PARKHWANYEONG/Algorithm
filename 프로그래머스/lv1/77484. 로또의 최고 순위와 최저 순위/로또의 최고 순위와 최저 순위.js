566b1e8150a6b8541d9217c3b6b979877595fa5b
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-28 21:11:02 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,14 +1,7 @@
 function solution(lottos, win_nums) {
-    let temp = { 6:1, 5:2, 4:3, 3:4, 2:5 ,1:6, 0:6 };
-    let include = 0;
-    let zero = lottos.filter(v=>v===0).length;
+    let hash = {6:1,5:2,4:3,3:4,2:5,1:6,0:6}
+    let zeroCnt = lottos.filter(v=> !v).length;
+    let include = lottos.filter(v=> win_nums.includes(v)).length;
     
-    for(let i=0; i<lottos.length; i++){
-        if(win_nums.includes(lottos[i])){
-            include++;
-        }
-    }
-    
-    return [temp[(include+zero)], temp[include]]
-
+    return [hash[(zeroCnt+include)] , hash[include]]
 }
