8cb7a866f5853b921d2fedbff583087a154d8780
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-04-28 16:37:07 +0900

    [level 1] Title: 소수 찾기, Time: 137.41 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,18 +1,16 @@
 function solution(n) {
     let set = new Set();
-    set.add(2);
     
-    for(let i=3; i<=n; i+=2){
-        set.add(i)
+    for(let i=1; i<=n; i+=2){
+        set.add(i);
     }
     
-    for(let j=3; j<=Math.sqrt(n); j++){
-        if(set.has(j)){
-            for(let k=j*2; k<=n; k+=j){
+    for(let j=2; j<=Math.sqrt(n); j++){
+            if(set.has(j))
+        for(let k=j*2; k<=n; k+=j){
                 set.delete(k);
-            }
         }
     }
     
-    return set.size
+   return set.size
 }
