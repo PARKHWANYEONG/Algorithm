ba4ebe4163595628bf95937e9fbed5e02de50b48
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-10-04 14:43:16 +0900

    [unrated] Title: x 사이의 개수, Time: 5.74 ms, Memory: 92.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String myString) {
+        String[] arr = myString.split("x",-1);
+        return Arrays.stream(arr).mapToInt(v->v.length()).toArray();
+    }
+}
