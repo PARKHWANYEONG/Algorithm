5bf69fca2e7ff96811353c43c54aec649a4f2e50
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-11-28 09:55:38 +0900

    [level 2] Title: 가장 큰 수, Time: 2.77 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        
+        String answer = Arrays
+            .stream(numbers)
+            .mapToObj(String::valueOf)
+            .sorted((a, b) -> (b + a).compareTo(a + b))
+            .reduce("", (a,b) -> a + b);
+        
+        if(answer.charAt(0) == '0'){
+            return "0";
+        }
+        
+        return answer;
+            
+    }
+}
