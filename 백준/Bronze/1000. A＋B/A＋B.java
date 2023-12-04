9d831306201313a93d3ae7f7fb56e0235a29d5ca
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-12-05 06:41:15 +0900

    [Bronze V] Title: A+B, Time: 128 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        System.out.println(Arrays.stream(input).mapToInt(Integer::parseInt).sum());
+    }
+}
