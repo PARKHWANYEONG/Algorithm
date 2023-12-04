908abc5b7037925599cc61c557c5c8c305fe3c86
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-12-05 08:33:44 +0900

    [Bronze III] Title: 알람 시계, Time: 156 ms, Memory: 16096 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,24 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int hour = scanner.nextInt();
-        int min = scanner.nextInt();
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        if (min < 45) {
-            if (hour - 1 < 0) {
-                hour = 23;
-            } else {
-                hour--;
-            }
-            min += 15;
-        } else {
-            min -= 45;
+        int time = H * 60 + M - 45;
+
+        if (time < 0) {
+            time += 1440;
         }
-        String answer = hour + " " + min;
-        
-        System.out.println(answer);
+        System.out.println(time/60 + " "+ time%60);
     }
 }
