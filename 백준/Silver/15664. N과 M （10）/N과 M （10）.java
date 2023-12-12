9d22d72d93ab8ca5113f43efd8c7c582b450f014
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-12-13 04:32:23 +0900

    [Silver II] Title: N과 M (10), Time: 160 ms, Memory: 16340 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedWriter bw;
+    static int N,M;
+    static int[] numbers;
+    static boolean[] isUsed;
+    static LinkedHashSet<String> set;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        numbers = new int[N];
+        isUsed = new boolean[N];
+        set = new LinkedHashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st1.nextToken());
+        }
+
+        Arrays.sort(numbers);
+
+        dfs(0,"",0);
+
+        for (String str : set) {
+            bw.write(str);
+            bw.newLine();
+        }
+
+        bw.flush();
+    }
+
+    private static void dfs(int cnt,String str,int index) throws IOException {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                set.add(str);
+            }
+            return;
+        }
+
+        for (int i = index; i < N; i++) {
+            dfs(cnt+1, str + numbers[i]+" ",i+1);
+        }
+    }
+}
