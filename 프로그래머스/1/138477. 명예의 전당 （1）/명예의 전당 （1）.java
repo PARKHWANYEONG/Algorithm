47bc350d2cbc9e7f7a49e8740c05e38b318abc9c
PARKHWANYEONG <120480770+PARKHWANYEONG@users.noreply.github.com>
2023-12-18 02:10:16 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.29 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,20 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        
+        for (int i = 0; i < score.length; i++) {
+            if (queue.size() < k) {
+                queue.add(score[i]);
+            } else if(queue.peek() < score[i]){
+                queue.add(score[i]);
+                queue.poll();
+            }
+            answer[i] = queue.peek();
+        }
+        return answer;
+    }
+}
